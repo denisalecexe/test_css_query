f6153051edfd2813dfa746ed0bd9abfd4ade6c3c
denisalecexe <deniszenomiteus@gmail.com>
2024-05-27 15:05:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/* manda a schermo le dimensioni intere dell'area occupata dalla 
+finestra espresssa in pixel grazie a 'innerWidth' e 'innerHeight' */
+document.getElementById("tester").innerHTML =
+innerWidth + "x" + innerHeight;
+
+/* cambia lo stile (colore) del testo */
+document.getElementById("tester").style.color='white';
+
+/* cambia lo stile (font size) del testo*/
+document.getElementById("tester").style.fontSize='20px';
+
+/* cambia lo stile (text align) del testo */
+document.getElementById("tester").style.textAlign='center';
